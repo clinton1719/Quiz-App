8e9cb1faf083697789c14f72cb8771999471a8ee
Clinton <clintoncleetus17@gmail.com>
2020-04-28 12:04:06 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/services.dart';
+import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+
+QuizBrain quizBrain = QuizBrain();
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  String c,w;
+  List <Icon> scoreKeeper = [];
+
+  void checkAnswer(bool ans) {
+    bool correctAnswer = quizBrain.getAnswer();
+    setState(() {
+      if (quizBrain.isFinished() == true) {
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz.',
+          buttons: [
+            DialogButton(
+              child: Text(
+                "Play Again",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () => Navigator.pop(context),
+              color: Color.fromRGBO(0, 179, 134, 1.0),
+            ),
+            DialogButton(
+              child: Text(
+                "Quit",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () => SystemNavigator.pop(),
+              color: Color.fromRGBO(0, 179, 134, 1.0),
+            )
+          ]
+        ).show();
+
+        quizBrain.reset();
+        scoreKeeper = [];
+      }
+      else {
+      if (ans == correctAnswer) {
+        scoreKeeper.add(Icon(Icons.check, color: Colors.green,));
+        winSound();
+      }
+      else {
+        scoreKeeper.add(Icon(Icons.close, color: Colors.red,));
+        failSound();
+      }
+      quizBrain.nextQuestion();
+    }
+  });
+  }
+
+  void winSound (){
+    final player = AudioCache();
+    player.play("win.mp3");
+  }
+
+  void failSound (){
+    final player = AudioCache();
+    player.play("fail.mp3");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 4,
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestion(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                  checkAnswer(true);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(false);
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ],
+    );
+  }
+}
+
